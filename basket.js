6539030e37a3b864695b86dc5bbb51ca50883859
ujjwal <ujjwalprajapati2872005@gmail.com>
2026-02-11 15:48:58 +0530

    all  done

---
+++ basket.js
@@ -0,0 +1,34 @@
+let h=document.getElementById("home")
+let g=document.getElementById("guest")
+let count1=0
+let count2=0
+function aa()
+{
+    count1+=1
+    h.textContent=count1
+}
+function ab()
+{
+    count1+=2
+    h.textContent=count1
+}
+function ac()
+{
+    count1+=3
+    h.textContent=count1
+}
+function ba()
+{
+    count2+=1
+    g.textContent=count2
+}
+function bb()
+{
+    count2+=2
+    g.textContent=count2
+}
+function bc()
+{
+    count2+=3
+    g.textContent=count2
+}
